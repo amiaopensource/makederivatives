eab465642c211d929fcb009d80cf1b2a857e2755
Dave Rice <dericed@gmail.com>
2013-04-03 00:47:37 -0400

    convert indenting space to tab

--- slatemezz.sh
+++ slatemezz.sh
@@ -7,8 +7,8 @@ OUTFILESUFFIX="_mezz.mp4"
 TMPSLATE="/tmp/slate.mov"
 
 cleanup(){
-  rm "$TMPSLATE"
-  exit $1
+	rm "$TMPSLATE"
+	exit $1
 }
 trap clean_up SIGHUP SIGINT SIGTERM
 
@@ -16,8 +16,8 @@ trap clean_up SIGHUP SIGINT SIGTERM
 [ ! -s "$SLATEFILE" ] && { echo I can\'t find this slate file you\'re referring to. ; exit 2 ;};
 
 while [ "$*" != "" ] ; do
-  ffmpeg -n -loop 1 -i "$SLATEFILE" -t 5 -f lavfi -i aevalsrc=0::d=5:s=44100 -vf 'scale=640:480,setdar=4/3,fps=fps=ntsc,fade=in:0:30,fade=out:120:30' -t 5 -r:v ntsc -c:v ffv1 -pix_fmt yuv420p -c:v libx264 -c:a libfaac -r:a 44100 -ac 2 "$TMPSLATE"
-  command="ffmpeg -i \"$TMPSLATE\" -vsync 0 -i \"$1\" -filter_complex '[1:v]scale=640:480:interl=1,setdar=4/3,yadif,format=yuv420p,fps=fps=ntsc[pvid];[0:v:0][0:a:0][pvid][1:a:0]concat=n=2:v=1:a=1[v][a]' -map '[v]' -map '[a]' -pix_fmt yuv420p -b:v 400k -c:v libx264 -c:a libfaac -r:a 44100 -ac 2 \"${1%.*}${OUTFILESUFFIX}\""
-  eval "$command"
-  shift
+	ffmpeg -n -loop 1 -i "$SLATEFILE" -t 5 -f lavfi -i aevalsrc=0::d=5:s=44100 -vf 'scale=640:480,setdar=4/3,fps=fps=ntsc,fade=in:0:30,fade=out:120:30' -t 5 -r:v ntsc -c:v ffv1 -pix_fmt yuv420p -c:v libx264 -c:a libfaac -r:a 44100 -ac 2 "$TMPSLATE"
+	command="ffmpeg -i \"$TMPSLATE\" -vsync 0 -i \"$1\" -filter_complex '[1:v]scale=640:480:interl=1,setdar=4/3,yadif,format=yuv420p,fps=fps=ntsc[pvid];[0:v:0][0:a:0][pvid][1:a:0]concat=n=2:v=1:a=1[v][a]' -map '[v]' -map '[a]' -pix_fmt yuv420p -b:v 400k -c:v libx264 -c:a libfaac -r:a 44100 -ac 2 \"${1%.*}${OUTFILESUFFIX}\""
+	eval "$command"
+	shift
 done
